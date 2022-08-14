964b28771b925abc9494ed28a59861ad472d1040
Majid <mrouhi13@gmail.com>
2022-08-15 00:47:45 +0430

    fix heading

--- components/post-title.tsx
+++ components/post-title.tsx
@@ -7,14 +7,14 @@ type Props = {
 
 const PostTitle = ({ slug, title }: Props) => {
     return (
-        <h3 className='text-3xl mx-5 dark:text-white'>
+        <h2 className='text-3xl mx-5 dark:text-white'>
             <Link
                 as={`/posts/${slug}`}
                 href='/posts/[slug]'
             >
                 <a>{title}</a>
             </Link>
-        </h3>
+        </h2>
     )
 }
 

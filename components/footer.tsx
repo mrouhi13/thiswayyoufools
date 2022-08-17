4e9afd249a9e3017f2b1c38eb3e2972fe722c0a9
Majid <mrouhi13@gmail.com>
2022-08-18 00:43:50 +0430

    improve footer text color

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     const thisYear = new Date().getFullYear()
     return (
-        <footer className='flex justify-center h-12 text-gray-400'>
+        <footer className='flex justify-center h-12 text-gray-500 dark:text-gray-400'>
             Majid Rouhi &copy; {thisYear}
         </footer>
     )

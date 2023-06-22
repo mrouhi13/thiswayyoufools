4f8b61d124bef008d45a8a76873848aeeb704e52
Majid <mrouhi13@gmail.com>
2023-06-23 01:03:42 +0330

    add analytics package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,30 @@
 import Head from 'next/head'
 import { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Head>
-                <meta charSet='utf-8'/>
-                <meta
-                    name='viewport'
-                    content='width=device-width, initial-scale=1, viewport-fit=cover'
-                />
-                <link
-                    rel='icon'
-                    type='image/svg+xml'
-                    href='/assets/favicon/favicon.svg'
-                />
-                <link
-                    rel='icon'
-                    type='image/png'
-                    href='/assets/favicon/favicon.png'
-                />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8"/>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/assets/favicon/favicon.svg"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/assets/favicon/favicon.png"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <Analytics/>
+    </>
+  )
 }

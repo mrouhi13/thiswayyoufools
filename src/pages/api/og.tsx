7aa284ff2f62bbb86cd8d9560a5d98f17ad7e54f
Majid <mrouhi13@gmail.com>
2023-06-25 15:08:09 +0330

    add og api

---
+++ src/pages/api/og.tsx
@@ -0,0 +1,47 @@
+import { ImageResponse } from '@vercel/og'
+import { NextRequest } from 'next/server'
+import BackgroundCanvas from '@/components/og-image-background'
+import Content from '@/components/og-content'
+
+export const config = {
+  runtime: 'edge'
+}
+
+const fetchFont = fetch(new URL('../../../public/fonts/JetBrainsMono-Regular.ttf', import.meta.url)).then(
+  (res) => res.arrayBuffer()
+)
+
+export default async function handler(request: NextRequest) {
+  const fontData = await fetchFont
+  const { searchParams } = request.nextUrl
+  const title = searchParams.get('title')
+  const author = searchParams.get('author')
+
+  try {
+    return new ImageResponse(
+      (
+        <BackgroundCanvas>
+          <Content
+            title={title ? title : 'Blog'}
+            author={author ? author : 'Majid Rouhi'}
+          />
+        </BackgroundCanvas>
+      ),
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: 'JetBrainsMono',
+            data: fontData,
+            style: 'normal'
+          }
+        ]
+      }
+    )
+  } catch {
+    return new Response(`Failed to generate the image`, {
+      status: 500
+    })
+  }
+}

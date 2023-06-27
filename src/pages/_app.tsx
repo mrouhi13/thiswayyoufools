a4b1c14ab1f0910936d990c607f94e342e068fff
Majid <mrouhi13@gmail.com>
2023-06-27 12:36:26 +0330

    move styles to src/

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
-import 'styles/globals.css'
+import '@/styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (

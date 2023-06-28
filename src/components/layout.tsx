57de81fc253873ef12468cde12626d122f94eb8a
Majid <mrouhi13@gmail.com>
2023-06-28 12:27:18 +0330

    update gtag script

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Footer from '@/components/footer'
 import Header from '@/components/header'
 import PageTitle from '@/components/page-title'
-import Script from 'next/script'
 
 type Props = {
   pageTitle: string
@@ -14,18 +13,6 @@ export default function Layout({ pageTitle, showBack, children }: Props) {
   return (
     <div className="dark:bg-dark">
       <Header/>
-      <Script
-        id="google_analytics"
-        dangerouslySetInnerHTML={{
-          __html: `window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-                            ga('create', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', 'auto');
-                            ga('send', 'pageview');`
-        }}
-      />
-      <Script
-        async
-        src="https://www.google-analytics.com/analytics.js"
-      ></Script>
       <main className="container-center flex-grow 2xl:mx-112 xl:mx-80 lg:mx-40 md:mx-32 sm:mx-16 my-24">
         <PageTitle showBack={showBack}>{pageTitle}</PageTitle>
         {children}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 import '@/styles/globals.css'
+import Script from 'next/script'
+import React from 'react'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -23,6 +25,17 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           href="/favicon.png"
         />
       </Head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-KLNTTNDRHV"
+      ></Script>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`}
+      </Script>
       <Component {...pageProps} />
       <Analytics/>
     </>

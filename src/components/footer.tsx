5d0ed87116d72b31c5fa91aa6c682dfce82b45f3
Majid <mrouhi13@gmail.com>
2023-09-07 10:45:32 +0330

    remove unused components and files

--- src/components/footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  const thisYear = new Date().getFullYear()
-  return (
-    <footer className="flex justify-center h-12 text-gray-500 dark:text-gray-400 text-xs font-mono">
-      Majid Rouhi &copy; {thisYear}
-    </footer>
-  )
-}

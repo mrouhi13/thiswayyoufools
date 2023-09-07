1d2f074359d7d44f9f8c5db01ea6cb7b3d5ac292
Majid <mrouhi13@gmail.com>
2023-09-07 10:50:58 +0330

    add post-body.tsx

---
+++ src/app/components/post-body.tsx
@@ -0,0 +1,12 @@
+import markdownStyles from '@/app/components/markdown-styles.module.css'
+import cn from 'classnames'
+
+type Props = {
+  content: string
+}
+
+export default function PostBody({ content }: Props) {
+  return (
+    <div className={cn(markdownStyles['markdown'], 'm-5 text-gray-800 dark:text-gray-100')} dangerouslySetInnerHTML={{ __html: content }}/>
+  )
+}
